b62bffb3d0c9ec33a0298a7efd1cc2b269fe2261
lprates05 <lorenapratesdl@outlook.com>
2025-04-23 21:27:24 +0000

    omnicalc

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,10 @@ Rails.application.routes.draw do
   get("/wizard_add", { :controller => "addition", :action => "add_these" })
 
   get("/subtract", { :controller => "subtraction", :action => "show_sub_form" })
-  get("", { :controller => "subtraction", :action => "subtract" })
+  get("/wizard_subtract", { :controller => "subtraction", :action => "subtract_these" })
 
-  get("/multiply", { :controller => "multiplication", :action => "multiplication" })
-  get("/wizard_multiply", { :controller => "multiplication", :action => "multiply" })
+  get("/multiply", { :controller => "multiplication", :action => "show_multiplication_form" })
+  get("/wizard_multiply", { :controller => "multiplication", :action => "multiply_these" })
 
   get("/divide", { :controller => "division", :action => "show_division_form" })
   get("/wizard_div", { :controller => "div", :action => "divide_these" })
